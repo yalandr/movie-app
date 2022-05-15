d9779d1738498937ca4835b2cfd343cf18f541e1
yalandr <52127093+yalandr@users.noreply.github.com>
2022-05-15 08:59:02 +0300

    Add files via upload

---
+++ static/1652594168/movies/676705/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fmovie-app\u002Fstatic\u002F1652594168",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmovies\u002F676705",config:{_app:{basePath:b,assetsPath:b,cdnURL:a}}}}(null,"\u002Fmovie-app\u002F"));
